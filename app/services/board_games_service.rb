14173e365d2799b34cd291231f1cb04a66ebe8eb
Lane Bretschneider <lanebret85@gmail.com>
2023-12-01 16:37:55 -0800

    feat: add service to consume BGG XML API

---
+++ app/services/board_games_service.rb
@@ -0,0 +1,16 @@
+class BoardGamesService
+  def get_board_games(game_ids)
+    response = conn.get("thing") do |req|
+      req.params["id"] = game_ids
+    end
+    Hash.from_xml(response.body)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "https://boardgamegeek.com/xmlapi2/") do |f|
+      f.headers["Content-Type"] = "application/xml"
+    end
+  end
+end
