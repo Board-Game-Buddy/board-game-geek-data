5a9ee1c997f51910158d246a409655557aa81e51
Connor Richmond <richmond.connor@gmail.com>
2023-12-05 02:19:58 -0500

    test: start test for boardgame service

---
+++ spec/services/board_game_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe BoardGamesService, type: :service do
+  describe '#get_board_games' do
+    it 'fetches board games from the API' do
+      stub_request(:get, "https://boardgamegeek.com/xmlapi2/thing?id=123,456").
+        to_return(status: 200, body: '<items><item id="123">...</item><item id="456">...</item></items>', headers: { 'Content-Type' => 'application/xml' })
+
+      service = BoardGamesService.new
+      result = service.get_board_games("123,456")
+
+      expect(result).to be_a(Hash)
+      expect(result['items']).to be_a(Hash)
+      expect(result['items']['item']).to be_an(Array)
+      expect(result['items']['item'].count).to eq(2)
+    end
+
+    it 'handles API errors gracefully' do
+      stub_request(:get, "https://boardgamegeek.com/xmlapi2/thing?id=789").
+        to_return(status: 404, body: 'Not Found', headers: { 'Content-Type' => 'text/plain' })
+
+      service = BoardGamesService.new
+      expect { service.get_board_games("789") }.to raise_error(BoardGamesService::ApiError)
+    end
+  end
+end
