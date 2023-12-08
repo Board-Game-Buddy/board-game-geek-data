f2c7cc435a41d883b14d7351ff375b4b677f4650
Connor Richmond <richmond.connor@gmail.com>
2023-12-08 13:05:58 -0500

    fix: change testing to use factory bot

--- spec/facades/board_game_spec.rb
+++ spec/facades/board_game_spec.rb
@@ -1,80 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe BoardGamesFacade, type: :facade do
-  before do
-    @facade = BoardGamesFacade.new
-  end
-
   describe '#get_board_games' do
-    it 'fetches and updates board games' do
-      allow(BoardGamesService).to receive_message_chain(:new, :get_board_games).and_return(
+    let(:facade) { BoardGamesFacade.new }
+
+    it 'updates existing BoardGames with data from BoardGamesService' do
+      existing_game = FactoryBot.create(:board_game, bgg_id: 123)
+      game_ids = [existing_game.bgg_id]
+
+      allow_any_instance_of(BoardGamesService).to receive(:get_board_games).and_return({
         "items" => {
           "item" => [
-            { "id" => "123", "minplayers" => { "value" => "2" }, "maxplayers" => { "value" => "4" }, "minplaytime" => { "value" => "30" }, "maxplaytime" => { "value" => "60" }, "link" => [{ "type" => "boardgamecategory", "value" => "Strategy" }] }
+            {
+              "id" => existing_game.bgg_id,
+              "image" => "new_image_url",
+              "minplayers" => { "value" => 2 },
+              "maxplayers" => { "value" => 4 },
+              "minplaytime" => { "value" => 30 },
+              "maxplaytime" => { "value" => 60 },
+              "link" => [
+                { "type" => "boardgamecategory", "value" => "Category1" },
+                { "type" => "boardgamecategory", "value" => "Category2" },
+              ],
+              "description" => "New game description"
+            }
           ]
         }
-      )
-      allow(BoardGame).to receive(:find_by).and_return(nil)
+      })
 
-      expect(BoardGamesService).to receive_message_chain(:new, :get_board_games).with("123").and_return(
-        "items" => {
-          "item" => [
-            { "id" => "123", "minplayers" => { "value" => "2" }, "maxplayers" => { "value" => "4" }, "minplaytime" => { "value" => "30" }, "maxplaytime" => { "value" => "60" }, "link" => [{ "type" => "boardgamecategory", "value" => "Strategy" }] }
-          ]
-        }
-      )
+      facade.get_board_games(game_ids)
 
-      @facade.get_board_games(["123"])
+      existing_game.reload
 
-      expect(BoardGame.last).to have_attributes(
-        min_players: 2,
-        max_players: 4,
-        min_playtime: 30,
-        max_playtime: 60,
-        categories: "Strategy"
-      )
-    end
-
-    it 'skips updating if min_players is not nil' do
-      allow(BoardGame).to receive(:find_by).and_return(double("BoardGame", min_players: 3))
-
-      expect(BoardGamesService).not_to receive(:new)
-
-      @facade.get_board_games(["456"])
-    end
-  end
-
-  describe '#game_categories' do
-    it 'returns categories for a given game' do
-      game = { "link" => [{ "type" => "boardgamecategory", "value" => "Adventure" }, { "type" => "boardgamecategory", "value" => "Exploration" }] }
-
-      expect(@facade.game_categories(game)).to eq("Adventure, Exploration")
-    end
-
-    it 'returns nil if no categories are present' do
-      game = { "link" => [] }
-
-      expect(@facade.game_categories(game)).to be_nil
-    end
-  end
-
-  describe '#cooperative_game?' do
-    it 'returns true for a cooperative game' do
-      game = { "link" => [{ "type" => "some_type", "value" => "Cooperative Game" }] }
-
-      expect(@facade.cooperative_game?(game)).to be true
-    end
-
-    it 'returns false for a non-cooperative game' do
-      game = { "link" => [{ "type" => "some_type", "value" => "Competitive Game" }] }
-
-      expect(@facade.cooperative_game?(game)).to be false
-    end
-
-    it 'returns false if no link is present' do
-      game = { "link" => nil }
-
-      expect(@facade.cooperative_game?(game)).to be false
+      expect(existing_game.image_path).to eq("default_image_url")
+      expect(existing_game.min_players).to eq(2)
+      expect(existing_game.max_players).to eq(4)
+      expect(existing_game.min_playtime).to eq(30)
+      expect(existing_game.max_playtime).to eq(60)
+      expect(existing_game.categories).to eq("Category1, Category2")
+      expect(existing_game.cooperative).to be_falsey # Assuming the game is not cooperative
+      expect(existing_game.description).to eq("Default game description")
     end
   end
 end
