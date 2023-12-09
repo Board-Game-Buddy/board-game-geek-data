566878cb06c62824b77e02df7f4b48fa4007180f
Lane Bretschneider <lanebret85@gmail.com>
2023-12-09 12:47:52 -0800

    Update board_games_controller.rb
    commented out pry removed

--- app/controllers/api/v1/board_games_controller.rb
+++ app/controllers/api/v1/board_games_controller.rb
@@ -29,7 +29,6 @@ class Api::V1::BoardGamesController < ApplicationController
   def all_by_params
     boardgames = filter_by_params(params)
     serialized_response = BoardGamesSerializer.new(boardgames).serializable_hash
-    # require 'pry';binding.pry
     render json: {
       current_page: params[:page] || "1",
       total_pages: boardgames.total_pages.to_s,
