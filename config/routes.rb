e72a7ebd8dd24c7f75ee663a4c352cfcc7468bf2
Lane Bretschneider <lanebret85@gmail.com>
2023-12-09 11:01:19 -0800

    Update routes.rb
    update routes to re-add lost routes, namespace all handrolled routes, and place above resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      get "/board_games/all_by_params", to: "board_games#all_by_params"
+      
+      get '/board_games', to: 'board_games#carousel'
+      
+      get '/multi_board_games', to: 'board_games#multi_games'
+      
       resources :board_games, only: [:show]
-      get '/board_games', to: '/api/v1/board_games#carousel'
     end
   end
-  
-  get '/api/v1/multi_board_games', to: 'api/v1/board_games#multi_games'
 end
