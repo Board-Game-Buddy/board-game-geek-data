9ab07258d1d49ff4aec6f39fd90a9175d3d39217
Connor Richmond <richmond.connor@gmail.com>
2023-12-09 18:15:36 -0500

    test covers a little more

--- spec/facades/board_game_facade_spec.rb
+++ spec/facades/board_game_facade_spec.rb
@@ -1,23 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe BoardGamesFacade, type: :facade do
-  before do
-    @existing_game = FactoryBot.create(:board_game, bgg_id: 123)
-    @game_ids = [@existing_game.bgg_id]
-    @facade = BoardGamesFacade.new
+
+  describe '#game_categories' do
+    it 'returns categories for a given game' do
+      game = { "link" => [{ "type" => "boardgamecategory", "value" => "Category1" }, { "type" => "boardgamecategory", "value" => "Category2" }] }
+      expect(BoardGamesFacade.new.game_categories(game)).to eq("Category1, Category2")
+    end
+
+    it 'returns nil if there are no categories' do
+      game = { "link" => [] }
+      expect(BoardGamesFacade.new.game_categories(game)).to be_nil
+    end
   end
 
-  it 'updates existing BoardGames with data from BoardGamesService' do
-    @facade.get_board_games(@game_ids)
-    @existing_game.reload
+  describe '#cooperative_game?' do
+    it 'returns true if the game is a cooperative game' do
+      game = { "link" => [{ "value" => "Cooperative Game" }] }
+      expect(BoardGamesFacade.new.cooperative_game?(game)).to be_truthy
+    end
 
-    expect(@existing_game.image_path).to start_with("https://example.com/mock_game")
-    expect(@existing_game.min_players).to be_an(Integer)
-    expect(@existing_game.max_players).to be_an(Integer)
-    expect(@existing_game.min_playtime).to be_an(Integer)
-    expect(@existing_game.max_playtime).to be_an(Integer)
-    expect(JSON.parse(@existing_game.categories)).to be_kind_of(Array)
-    expect(@existing_game.cooperative).to be_in([true, false])
-    expect(@existing_game.description).to be_a(String)
+    it 'returns false if the game is not a cooperative game' do
+      game = { "link" => [] }
+      expect(BoardGamesFacade.new.cooperative_game?(game)).to be_falsey
+    end
   end
 end
