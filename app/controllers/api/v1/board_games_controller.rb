26d53dfb94c69d8ca5a84a3992da8068785faaf5
Reed Hillmar <reedhillmar@gmail.com>
2024-01-23 17:38:15 -0700

    refactors filter_by_params method to allow inclusive matching of categories

--- app/controllers/api/v1/board_games_controller.rb
+++ app/controllers/api/v1/board_games_controller.rb
@@ -125,17 +125,34 @@ class Api::V1::BoardGamesController < ApplicationController
   end
 
   def filter_by_params(params)
-    categories = params[:categories]
-    min_players = params[:min_players]
-    max_players = params[:max_players]
-    cooperative = params[:cooperative]
     board_games = BoardGame.all
-    board_games = board_games.where('min_players >= ?', min_players) if min_players
-    board_games = board_games.where('max_players <= ?', max_players) if max_players
-    board_games = board_games.where(cooperative: true) if cooperative
-    categories.split(',').each do |category|
-      board_games = board_games.where("categories ILIKE ?", "%#{category}%")
-    end if categories
+    board_games = board_games.where('min_players >= ?', params[:min_players]) if params[:min_players]
+    board_games = board_games.where('max_players <= ?', params[:max_players]) if params[:max_players]
+    board_games = board_games.where('min_playtime >= ?', params[:min_playtime]) if params[:min_playtime]
+    board_games = board_games.where('max_playtime <= ?', params[:max_playtime]) if params[:max_playtime]
+    board_games = board_games.where(cooperative: params[:cooperative]) if params[:cooperative]
+
+    ### THIS SOLUTION IS EXCLUSIVE - GAMES THAT MATCH ALL CATEGORIES
+    # params[:categories].split(',').each do |category|
+    #   board_games = board_games.where("categories ILIKE ?", "%#{category}%")
+    # end if params[:categories]
+
+    ### THIS SOLUTION IS INCLUSIVE - GAMES THAT MATCH ANY CATEGORY
+    if params[:categories]
+      # make a new hash to store games by category
+      category_games = {}
+
+      # populate games by category
+      params[:categories].split(',').each do |category|
+        category_games[category.to_sym] = board_games.where("categories ILIKE ?", "%#{category}%")
+      end
+      
+      # combine all category games into single array
+      category_games = category_games.values.flatten.uniq
+
+      # use category games array to make an AR query
+      board_games = BoardGame.where(id: category_games.map {|game| game.id})
+    end
 
     board_games.order(:rank).paginate(page: params[:page])
   end
